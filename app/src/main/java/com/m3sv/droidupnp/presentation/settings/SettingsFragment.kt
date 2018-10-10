6ecc6febb6b1ef12947b0c9e36972a08f374dedb
m3sv <mesmeriz3@gmail.com>
2018-10-11 00:06:50 +0300

    Add preferences screen

--- app/src/main/java/com/m3sv/droidupnp/presentation/settings/SettingsFragment.kt
+++ app/src/main/java/com/m3sv/droidupnp/presentation/settings/SettingsFragment.kt
@@ -1,50 +1,16 @@
 package com.m3sv.droidupnp.presentation.settings
 
 import android.os.Bundle
-import android.preference.PreferenceManager
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.support.v7.preference.PreferenceFragmentCompat
 import com.m3sv.droidupnp.R
-import com.m3sv.droidupnp.databinding.SettingsFragmentBinding
-import com.m3sv.droidupnp.presentation.base.BaseActivity
-import com.m3sv.droidupnp.presentation.base.BaseFragment
-import com.m3sv.droidupnp.presentation.base.THEME_KEY
 
 
-class SettingsFragment : BaseFragment() {
+class SettingsFragment : PreferenceFragmentCompat() {
 
 
-    private lateinit var binding: SettingsFragmentBinding
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        binding = SettingsFragmentBinding.inflate(inflater, container, false)
-        return binding.root
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        addPreferencesFromResource(R.xml.settings)
     }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        binding.changeTheme.setOnClickListener {
-            with(requireActivity() as BaseActivity) {
-                if (isLightTheme) {
-                    setTheme(R.style.AppTheme_Dark)
-                    PreferenceManager.getDefaultSharedPreferences(this).edit()
-                        .putBoolean(THEME_KEY, false).apply()
-                    recreate()
-                } else {
-                    setTheme(R.style.AppTheme)
-                    PreferenceManager.getDefaultSharedPreferences(this).edit()
-                        .putBoolean(THEME_KEY, true).apply()
-                    recreate()
-                }
-            }
-        }
-    }
-
 
     companion object {
         val TAG: String = SettingsFragment::class.java.simpleName
