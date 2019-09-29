4b5d7084f7174a9b1a52b00a47b433197e644096
m3sv <m3sv.dev@gmail.com>
2019-09-29 14:50:19 +0200

    Formatting

--- upnp/src/main/java/com/m3sv/plainupnp/upnp/MediaServer.kt
+++ upnp/src/main/java/com/m3sv/plainupnp/upnp/MediaServer.kt
@@ -10,28 +10,31 @@ import java.io.File
 
 
 class MediaServer(private val context: Context) :
-        SimpleWebServer(null, PORT, null, true) {
+    SimpleWebServer(null, PORT, null, true) {
 
     private val objectMap: MutableMap<String, ServerObject> = mutableMapOf()
 
     override fun serve(
-            uri: String,
-            method: Method,
-            header: Map<String, String>,
-            parms: Map<String, String>,
-            files: Map<String, String>
+        uri: String,
+        method: Method,
+        header: Map<String, String>,
+        parms: Map<String, String>,
+        files: Map<String, String>
     ): Response? {
         Timber.i("Serve uri: $uri")
         return try {
             val obj = getFileServerObject(uri)
 
-            Timber.i("Will serve " + obj.path)
+            Timber.i("Will serve: %s", obj.path)
 
             serveFile(File(obj.path), obj.mime, header).apply {
                 addHeader("realTimeInfo.dlna.org", "DLNA.ORG_TLAG=*")
                 addHeader("contentFeatures.dlna.org", "")
                 addHeader("transferMode.dlna.org", "Streaming")
-                addHeader("Server", "DLNADOC/1.50 UPnP/1.0 Cling/2.0 PlainUPnP/" + "0.0" + " Android/" + Build.VERSION.RELEASE)
+                addHeader(
+                    "Server",
+                    "DLNADOC/1.50 UPnP/1.0 Cling/2.0 PlainUPnP/" + "0.0" + " Android/" + Build.VERSION.RELEASE
+                )
             }
         } catch (e: InvalidIdentifierException) {
             Response(Response.Status.NOT_FOUND, MIME_PLAINTEXT, "Error 404, file not found.")
@@ -76,22 +79,28 @@ class MediaServer(private val context: Context) :
                 else -> null
             }?.let { contentUri ->
                 val columns =
-                        arrayOf(MediaStore.MediaColumns.DATA, MediaStore.MediaColumns.MIME_TYPE)
+                    arrayOf(
+                        MediaStore.MediaColumns.DATA,
+                        MediaStore.MediaColumns.MIME_TYPE
+                    )
                 val where = MediaStore.MediaColumns._ID + "=?"
                 val whereVal = arrayOf("" + mediaId)
 
 
-                context.contentResolver.query(contentUri, columns, where, whereVal, null)?.use { cursor ->
-                    if (cursor.moveToFirst()) {
-                        val mime = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.MediaColumns.MIME_TYPE))
+                context.contentResolver.query(contentUri, columns, where, whereVal, null)
+                    ?.use { cursor ->
+                        if (cursor.moveToFirst()) {
+                            val mime =
+                                cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.MediaColumns.MIME_TYPE))
 
-                        cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.MediaColumns.DATA))?.let { path ->
-                            val result = ServerObject(path, mime)
-                            objectMap[id] = result
-                            return result
+                            cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.MediaColumns.DATA))
+                                ?.let { path ->
+                                    val result = ServerObject(path, mime)
+                                    objectMap[id] = result
+                                    return result
+                                }
                         }
                     }
-                }
 
             }
 
