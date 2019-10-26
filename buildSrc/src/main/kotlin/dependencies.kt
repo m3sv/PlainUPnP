19cb6d0b5219534fef2e4a1aac089e453c95f951
m3sv <m3sv.dev@gmail.com>
2019-10-26 18:54:22 +0200

    Update and organize dependencies

--- buildSrc/src/main/kotlin/dependencies.kt
+++ buildSrc/src/main/kotlin/dependencies.kt
@@ -6,7 +6,7 @@ object Versions {
     const val buildTools = "29.0.0"
     const val minSdk = 21
     const val targetSdk = 29
-    const val supportLibrary = "1.0.0"
+    const val supportLibrary = "1.1.0"
     const val architectureComponents = "2.0.0"
     const val dagger = "2.24"
     const val glide = "4.9.0"
@@ -21,43 +21,42 @@ object Dependencies {
     const val kotlin = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${Versions.kotlin}"
     const val timber = "com.jakewharton.timber:timber:4.7.1"
 
+    val androidx = mapOf(
+        "appCompat" to "androidx.appcompat:appcompat:${Versions.supportLibrary}",
+        "cardView" to "androidx.cardview:cardview:1.0.0",
+        "constraintLayout" to "androidx.constraintlayout:constraintlayout:2.0.0-beta3",
+        "material" to "com.google.android.material:material:1.2.0-alpha01",
+        "lifecycle" to "androidx.lifecycle:lifecycle-extensions:${Versions.architectureComponents}",
+        "navigation" to mapOf(
+            "fragment" to "androidx.navigation:navigation-fragment-ktx:${Versions.navigation}",
+            "ui" to "androidx.navigation:navigation-ui-ktx:${Versions.navigation}"
+        ),
+        "preference" to "androidx.preference:preference:${Versions.supportLibrary}",
+        "recyclerView" to "androidx.recyclerview:recyclerview:1.0.0"
+    )
+
     val coroutines = mapOf(
         "core" to "org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.0",
         "rxJava2" to "org.jetbrains.kotlinx:kotlinx-coroutines-rx2:1.3.0"
     )
 
-    val rx = mapOf(
-        "kotlin" to "com.github.ReactiveX:rxKotlin:2.3.0",
-        "android" to "io.reactivex.rxjava2:rxandroid:2.0.1"
-    )
-
-    val androidx = mapOf(
-        "navigation" to mapOf(
-            "fragment" to "androidx.navigation:navigation-fragment-ktx:${Versions.navigation}",
-            "ui" to "androidx.navigation:navigation-ui-ktx:${Versions.navigation}"
-        ),
-        "lifecycle" to "androidx.lifecycle:lifecycle-extensions:${Versions.architectureComponents}",
-        "appCompat" to "androidx.appcompat:appcompat:${Versions.supportLibrary}",
-        "v4" to "androidx.legacy:legacy-support-v4:${Versions.supportLibrary}",
-        "design" to "com.google.android.material:material:${Versions.supportLibrary}",
-        "recyclerView" to "androidx.recyclerview:recyclerview:${Versions.supportLibrary}",
-        "cardView" to "androidx.cardview:cardview:${Versions.supportLibrary}",
-        "constraintLayout" to "androidx.constraintlayout:constraintlayout:2.0.0-beta2",
-        "preferences" to "androidx.preference:preference:${Versions.supportLibrary}"
-    )
-
     val dagger = mapOf(
-        "core" to "com.google.dagger:dagger:${Versions.dagger}",
-        "compiler" to "com.google.dagger:dagger-compiler:${Versions.dagger}",
-        "annotation" to "javax.inject:javax.inject:1",
         "android" to "com.google.dagger:dagger-android:${Versions.dagger}",
         "androidSupport" to "com.google.dagger:dagger-android-support:${Versions.dagger}",
-        "androidProcessor" to "com.google.dagger:dagger-android-processor:${Versions.dagger}"
+        "annotation" to "javax.inject:javax.inject:1",
+        "androidProcessor" to "com.google.dagger:dagger-android-processor:${Versions.dagger}",
+        "compiler" to "com.google.dagger:dagger-compiler:${Versions.dagger}",
+        "core" to "com.google.dagger:dagger:${Versions.dagger}"
     )
 
     val glide = mapOf(
-        "core" to "com.github.bumptech.glide:glide:${Versions.glide}",
-        "compiler" to "com.github.bumptech.glide:compiler:${Versions.glide}"
+        "compiler" to "com.github.bumptech.glide:compiler:${Versions.glide}",
+        "core" to "com.github.bumptech.glide:glide:${Versions.glide}"
+    )
+
+    val rx = mapOf(
+        "android" to "io.reactivex.rxjava2:rxandroid:2.0.1",
+        "kotlin" to "com.github.ReactiveX:rxKotlin:2.3.0"
     )
 
     val test = mapOf(
