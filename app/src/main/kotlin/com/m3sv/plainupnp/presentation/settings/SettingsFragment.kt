c9a4365f51f464889eeb2cfd059cbe3c486888ca
m3sv <m3sv.dev@gmail.com>
2019-11-24 13:42:22 +0100

    PU-000: Cleanup settings fragment

--- app/src/main/kotlin/com/m3sv/plainupnp/presentation/settings/SettingsFragment.kt
+++ app/src/main/kotlin/com/m3sv/plainupnp/presentation/settings/SettingsFragment.kt
@@ -13,6 +13,7 @@ import com.m3sv.plainupnp.R
 import com.m3sv.plainupnp.upnp.UpnpManager
 import dagger.android.support.DaggerAppCompatActivity
 import javax.inject.Inject
+import kotlin.LazyThreadSafetyMode.NONE
 
 
 class SettingsFragment : PreferenceFragmentCompat(),
@@ -21,50 +22,30 @@ class SettingsFragment : PreferenceFragmentCompat(),
     @Inject
     lateinit var upnpManager: UpnpManager
 
-    private val darkThemeKey by lazy(LazyThreadSafetyMode.NONE) { getString(R.string.dark_theme_key) }
-
     private val appVersion: String
         get() = requireActivity()
             .packageManager
             .getPackageInfo(requireActivity().packageName, 0)
             .versionName
 
+    private val darkThemeKey by lazy(NONE) {
+        getString(R.string.dark_theme_key)
+    }
+
     private val preferenceClickListener = Preference.OnPreferenceClickListener { preference ->
         when (preference.key) {
-            "rate" -> {
-                try {
-                    startActivity(
-                        Intent(
-                            Intent.ACTION_VIEW,
-                            Uri.parse("market://details?id=" + this.activity?.packageName)
-                        )
-                    )
-                } catch (e: Throwable) {
-                    startActivity(
-                        Intent(
-                            Intent.ACTION_VIEW,
-                            Uri.parse("http://play.google.com/store/apps/details?id=" + this.activity?.packageName)
-                        )
-                    )
-                }
-
+            RATE -> {
+                rateApplication()
                 true
             }
 
-            "github" -> {
-                val browserIntent =
-                    Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/m3sv/PlainUPnP"))
-                startActivity(browserIntent)
+            GITHUB -> {
+                openGithub()
                 true
             }
 
-            "privacy_policy" -> {
-                val browserIntent =
-                    Intent(
-                        Intent.ACTION_VIEW,
-                        Uri.parse("https://www.freeprivacypolicy.com/privacy/view/bf0284b77ca1af94b405030efd47d254")
-                    )
-                startActivity(browserIntent)
+            PRIVACY_POLICY -> {
+                openPrivacyPolicy()
                 true
             }
 
@@ -76,15 +57,13 @@ class SettingsFragment : PreferenceFragmentCompat(),
         (activity as? DaggerAppCompatActivity)?.androidInjector()?.inject(this)
         super.onViewCreated(view, savedInstanceState)
 
-        findPreference<Preference>("version")?.summary = appVersion
-        findPreference<Preference>("rate")?.onPreferenceClickListener = preferenceClickListener
-        findPreference<Preference>("github")?.onPreferenceClickListener = preferenceClickListener
-        findPreference<Preference>("privacy_policy")?.onPreferenceClickListener =
+        findPreference<Preference>(VERSION)?.summary = appVersion
+        findPreference<Preference>(RATE)?.onPreferenceClickListener = preferenceClickListener
+        findPreference<Preference>(GITHUB)?.onPreferenceClickListener = preferenceClickListener
+        findPreference<Preference>(PRIVACY_POLICY)?.onPreferenceClickListener =
             preferenceClickListener
 
-        listView.layoutParams = (listView.layoutParams as ViewGroup.MarginLayoutParams).apply {
-            bottomMargin = 64.toPx()
-        }
+        applyBottomMargin()
     }
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
@@ -103,21 +82,69 @@ class SettingsFragment : PreferenceFragmentCompat(),
 
     override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences, key: String) {
         if (key == darkThemeKey) {
-            when (sharedPreferences.getBoolean(darkThemeKey, false)) {
-                true -> {
-                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
-                    activity?.recreate()
-                }
+            val defaultNightMode = if (sharedPreferences.getBoolean(darkThemeKey, false))
+                AppCompatDelegate.MODE_NIGHT_YES
+            else
+                AppCompatDelegate.MODE_NIGHT_NO
 
-                false -> {
-                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-                    activity?.recreate()
-                }
-            }
+            AppCompatDelegate.setDefaultNightMode(defaultNightMode)
         }
     }
 
+    private fun openPrivacyPolicy() {
+        val browserIntent =
+            Intent(
+                Intent.ACTION_VIEW,
+                Uri.parse("https://www.freeprivacypolicy.com/privacy/view/bf0284b77ca1af94b405030efd47d254")
+            )
+        startActivity(browserIntent)
+    }
+
+    private fun openGithub() {
+        val browserIntent =
+            Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/m3sv/PlainUPnP"))
+        startActivity(browserIntent)
+    }
+
+    private fun openPlayMarketFallBack() {
+        startActivity(
+            Intent(
+                Intent.ACTION_VIEW,
+                Uri.parse("http://play.google.com/store/apps/details?id=" + this.activity?.packageName)
+            )
+        )
+    }
+
+    private fun openPlayMarket() {
+        startActivity(
+            Intent(
+                Intent.ACTION_VIEW,
+                Uri.parse("market://details?id=" + this.activity?.packageName)
+            )
+        )
+    }
+
+    private fun rateApplication() {
+        try {
+            openPlayMarket()
+        } catch (e: Throwable) {
+            openPlayMarketFallBack()
+        }
+    }
+
+    private fun applyBottomMargin() {
+        listView.layoutParams = (listView.layoutParams as ViewGroup.MarginLayoutParams).apply {
+            bottomMargin = 64.toPx()
+        }
+    }
 
     private fun Int.toPx(): Int =
         (this * this@SettingsFragment.resources.displayMetrics.density).toInt()
+
+    private companion object {
+        private const val VERSION = "version"
+        private const val RATE = "rate"
+        private const val GITHUB = "github"
+        private const val PRIVACY_POLICY = "privacy_policy"
+    }
 }
