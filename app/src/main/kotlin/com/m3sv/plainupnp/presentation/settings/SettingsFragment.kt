2e31f9443fae022d2c0568851535c1149a5eb744
m3sv <m3sv.dev@gmail.com>
2019-12-25 20:42:16 +0100

    Cleanup settings fragment

--- app/src/main/kotlin/com/m3sv/plainupnp/presentation/settings/SettingsFragment.kt
+++ app/src/main/kotlin/com/m3sv/plainupnp/presentation/settings/SettingsFragment.kt
@@ -6,61 +6,38 @@ import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.fragment.app.Fragment
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import com.m3sv.plainupnp.R
 import com.m3sv.plainupnp.presentation.main.MainActivity
 import com.m3sv.plainupnp.upnp.UpnpManager
 import javax.inject.Inject
-import kotlin.LazyThreadSafetyMode.NONE
 
+private val Fragment.packageName
+    get() = requireActivity().packageName
+
+private val Fragment.appVersion: String
+    get() = requireActivity()
+        .packageManager
+        .getPackageInfo(requireActivity().packageName, 0)
+        .versionName
 
 class SettingsFragment : PreferenceFragmentCompat(),
-    SharedPreferences.OnSharedPreferenceChangeListener {
+    SharedPreferences.OnSharedPreferenceChangeListener,
+    Preference.OnPreferenceClickListener {
 
     @Inject
     lateinit var upnpManager: UpnpManager
-
-    private val appVersion: String
-        get() = requireActivity()
-            .packageManager
-            .getPackageInfo(requireActivity().packageName, 0)
-            .versionName
-
-    private val darkThemeKey by lazy(NONE) {
-        getString(R.string.dark_theme_key)
-    }
-
-    private val preferenceClickListener = Preference.OnPreferenceClickListener { preference ->
-        when (preference.key) {
-            RATE -> {
-                rateApplication()
-                true
-            }
-
-            GITHUB -> {
-                openGithub()
-                true
-            }
-
-            PRIVACY_POLICY -> {
-                openPrivacyPolicy()
-                true
-            }
-
-            else -> false
-        }
-    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         (activity as MainActivity).mainActivitySubComponent.inject(this)
         super.onViewCreated(view, savedInstanceState)
 
         findPreference<Preference>(VERSION)?.summary = appVersion
-        findPreference<Preference>(RATE)?.onPreferenceClickListener = preferenceClickListener
-        findPreference<Preference>(GITHUB)?.onPreferenceClickListener = preferenceClickListener
-        findPreference<Preference>(PRIVACY_POLICY)?.onPreferenceClickListener =
-            preferenceClickListener
+        findPreference<Preference>(RATE)?.onPreferenceClickListener = this
+        findPreference<Preference>(GITHUB)?.onPreferenceClickListener = this
+        findPreference<Preference>(PRIVACY_POLICY)?.onPreferenceClickListener = this
     }
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
@@ -78,61 +55,64 @@ class SettingsFragment : PreferenceFragmentCompat(),
     }
 
     override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences, key: String) {
-        if (key == darkThemeKey) {
-            val defaultNightMode = if (sharedPreferences.getBoolean(darkThemeKey, false))
-                AppCompatDelegate.MODE_NIGHT_YES
-            else
-                AppCompatDelegate.MODE_NIGHT_NO
+        val darkThemeKey = getString(R.string.dark_theme_key)
+        when (key) {
+            darkThemeKey -> {
+                val defaultNightMode = if (isDarkThemeSet(sharedPreferences, darkThemeKey))
+                    AppCompatDelegate.MODE_NIGHT_YES
+                else
+                    AppCompatDelegate.MODE_NIGHT_NO
 
-            AppCompatDelegate.setDefaultNightMode(defaultNightMode)
+                AppCompatDelegate.setDefaultNightMode(defaultNightMode)
+            }
         }
     }
 
-    private fun openPrivacyPolicy() {
-        val browserIntent =
-            Intent(
-                Intent.ACTION_VIEW,
-                Uri.parse("https://www.freeprivacypolicy.com/privacy/view/bf0284b77ca1af94b405030efd47d254")
-            )
-        startActivity(browserIntent)
-    }
-
-    private fun openGithub() {
-        val browserIntent =
-            Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/m3sv/PlainUPnP"))
-        startActivity(browserIntent)
-    }
-
-    private fun openPlayMarketFallBack() {
-        startActivity(
-            Intent(
-                Intent.ACTION_VIEW,
-                Uri.parse("http://play.google.com/store/apps/details?id=" + this.activity?.packageName)
-            )
-        )
-    }
-
-    private fun openPlayMarket() {
-        startActivity(
-            Intent(
-                Intent.ACTION_VIEW,
-                Uri.parse("market://details?id=" + this.activity?.packageName)
-            )
-        )
-    }
-
-    private fun rateApplication() {
-        try {
-            openPlayMarket()
-        } catch (e: Throwable) {
-            openPlayMarketFallBack()
+    override fun onPreferenceClick(preference: Preference): Boolean {
+        val intent: Intent? = when (preference.key) {
+            RATE -> rateApplication()
+            GITHUB -> githubIntent()
+            PRIVACY_POLICY -> privacyPolicyIntent()
+            else -> null
         }
+
+        if (intent != null)
+            startActivity(intent)
+
+        return intent != null
     }
+
+    private fun privacyPolicyIntent() = Intent(Intent.ACTION_VIEW, Uri.parse(PRIVACY_POLICY_LINK))
+
+    private fun githubIntent() = Intent(Intent.ACTION_VIEW, Uri.parse(GITHUB_LINK))
+
+    private fun playMarketIntent() =
+        Intent(Intent.ACTION_VIEW, Uri.parse("$MARKET_PREFIX$packageName"))
+
+    private fun playMarketFallbackIntent() =
+        Intent(Intent.ACTION_VIEW, Uri.parse("$PLAY_STORE_PREFIX$packageName"))
+
+    private fun rateApplication(): Intent = try {
+        playMarketIntent()
+    } catch (e: Throwable) {
+        playMarketFallbackIntent()
+    }
+
+    private fun isDarkThemeSet(
+        sharedPreferences: SharedPreferences,
+        darkThemeKey: String
+    ) = sharedPreferences.getBoolean(darkThemeKey, false)
 
     private companion object {
         private const val VERSION = "version"
         private const val RATE = "rate"
         private const val GITHUB = "github"
         private const val PRIVACY_POLICY = "privacy_policy"
+
+        private const val GITHUB_LINK = "https://github.com/m3sv/PlainUPnP"
+        private const val PRIVACY_POLICY_LINK =
+            "https://www.freeprivacypolicy.com/privacy/view/bf0284b77ca1af94b405030efd47d254"
+        private const val PLAY_STORE_PREFIX = "http://play.google.com/store/apps/details?id="
+        private const val MARKET_PREFIX = "market://details?id="
     }
 }
