33be2ad030449029ec669446736c4bd01e7ffa5d
m3sv <m3sv.dev@gmail.com>
2020-01-18 17:49:12 +0100

    Expose flow instead of channel

--- app/src/main/kotlin/com/m3sv/plainupnp/presentation/main/FilterDelegate.kt
+++ app/src/main/kotlin/com/m3sv/plainupnp/presentation/main/FilterDelegate.kt
@@ -2,7 +2,8 @@ package com.m3sv.plainupnp.presentation.main
 
 import kotlinx.coroutines.channels.BroadcastChannel
 import kotlinx.coroutines.channels.Channel.Factory.BUFFERED
-import kotlinx.coroutines.channels.ReceiveChannel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.asFlow
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -11,7 +12,7 @@ class FilterDelegate @Inject constructor() {
 
     private val _state = BroadcastChannel<String>(BUFFERED)
 
-    val state: ReceiveChannel<String> = _state.openSubscription()
+    val state: Flow<String> = _state.asFlow()
 
     suspend fun filter(text: String) {
         _state.send(text)
