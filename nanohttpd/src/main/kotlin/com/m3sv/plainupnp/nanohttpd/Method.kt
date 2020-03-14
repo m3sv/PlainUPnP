2651cd4d634b008340861dbea5ea9ce3e5644432
m3sv <m3sv.dev@gmail.com>
2020-03-14 17:32:49 +0100

    Reformat Method

--- nanohttpd/src/main/kotlin/com/m3sv/plainupnp/nanohttpd/Method.kt
+++ nanohttpd/src/main/kotlin/com/m3sv/plainupnp/nanohttpd/Method.kt
@@ -4,10 +4,15 @@ package com.m3sv.plainupnp.nanohttpd
  * HTTP Request methods, with the ability to decode a `String` back to its enum value.
  */
 enum class Method {
-    GET, PUT, POST, DELETE, HEAD;
+    GET,
+    PUT,
+    POST,
+    DELETE,
+    HEAD;
 
     companion object {
         @JvmStatic
-        fun lookup(method: String): Method? = values().find { it.toString().equals(method, ignoreCase = true) }
+        fun lookup(method: String): Method? =
+            values().find { it.toString().equals(method, ignoreCase = true) }
     }
 }
