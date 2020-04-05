e8d943ba8617c60b2c311f9d75e13d46c8a40853
m3sv <m3sv.dev@gmail.com>
2020-04-05 16:39:49 +0200

    Add simple benchmark function

---
+++ common/src/main/kotlin/com/m3sv/plainupnp/common/utils/benchmarkUtil.kt
@@ -0,0 +1,14 @@
+package com.m3sv.plainupnp.common.utils
+
+import timber.log.Timber
+import java.util.concurrent.TimeUnit
+
+inline fun <T> benchmark(name: String, block: () -> T): T {
+    val start = System.nanoTime()
+    val result = block()
+    val end = System.nanoTime()
+
+    Timber.tag("PlainBenchmark").d("$name took ${TimeUnit.NANOSECONDS.toMillis(end - start)} ms")
+
+    return result
+}
