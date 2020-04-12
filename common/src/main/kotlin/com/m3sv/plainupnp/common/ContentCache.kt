1af21db79c1c06fd8315a74ce69df9f66493a1ff
m3sv <m3sv.dev@gmail.com>
2020-04-12 20:15:24 +0200

    Use default action_view instead of intent chooser when launching locally

--- common/src/main/kotlin/com/m3sv/plainupnp/common/ContentCache.kt
+++
@@ -1,8 +0,0 @@
-package com.m3sv.plainupnp.common
-
-import android.util.LruCache
-import javax.inject.Inject
-import javax.inject.Singleton
-
-@Singleton
-class ContentCache @Inject constructor() : LruCache<String, String>(10 * 1024 * 1024)
--- upnp/src/main/kotlin/com/m3sv/plainupnp/upnp/usecase/LaunchLocallyUseCase.kt
+++ upnp/src/main/kotlin/com/m3sv/plainupnp/upnp/usecase/LaunchLocallyUseCase.kt
@@ -3,7 +3,6 @@ package com.m3sv.plainupnp.upnp.usecase
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
-import com.m3sv.plainupnp.common.ContentCache
 import com.m3sv.plainupnp.upnp.didl.ClingAudioItem
 import com.m3sv.plainupnp.upnp.didl.ClingImageItem
 import com.m3sv.plainupnp.upnp.didl.ClingVideoItem
@@ -11,17 +10,10 @@ import com.m3sv.plainupnp.upnp.manager.RenderItem
 import timber.log.Timber
 import javax.inject.Inject
 
-data class LocalModel(
-    val uri: String,
-    val contentType: String
-)
-
-class LaunchLocallyUseCase @Inject constructor(
-    private val context: Context,
-    private val contentCache: ContentCache
-) {
+class LaunchLocallyUseCase @Inject constructor(private val context: Context) {
     fun execute(item: RenderItem) {
-        item.item.uri?.let { uri ->
+        val uri = item.item.uri
+        if (uri != null) {
             val contentType = when (item.item) {
                 is ClingAudioItem -> "audio/*"
                 is ClingImageItem -> "image/*"
@@ -29,22 +21,14 @@ class LaunchLocallyUseCase @Inject constructor(
                 else -> null
             }
 
-            contentType?.let {
-                LocalModel(
-                    contentCache.get(item.item.id) ?: uri,
-                    contentType
-                )
-            }?.let {
+            if (contentType != null) {
                 try {
-                    val intent = Intent(Intent.ACTION_VIEW, Uri.parse(it.uri)).apply {
-                        setDataAndType(Uri.parse(it.uri), it.contentType)
+                    val intent = Intent(Intent.ACTION_VIEW, Uri.parse(uri)).apply {
+                        setDataAndType(Uri.parse(uri), contentType)
+                        flags += Intent.FLAG_ACTIVITY_NEW_TASK
                     }
-                    // TODO Add title
-//                    val title = context.getString(R.string.play_with)
-                    val chooser = Intent.createChooser(intent, "")
-                        .apply { flags += Intent.FLAG_ACTIVITY_NEW_TASK }
                     if (intent.resolveActivity(context.packageManager) != null) {
-                        context.startActivity(chooser)
+                        context.startActivity(intent)
                     }
                 } catch (e: Exception) {
                     Timber.e(e)
