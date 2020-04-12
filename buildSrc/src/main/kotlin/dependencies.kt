26937f8d796724d7b67b496f7f865222764b90ef
m3sv <m3sv.dev@gmail.com>
2020-04-12 19:44:31 +0200

    Bump architecture components version

--- buildSrc/src/main/kotlin/dependencies.kt
+++ buildSrc/src/main/kotlin/dependencies.kt
@@ -1,5 +1,5 @@
 object Versions {
-    const val architectureComponents = "2.1.0"
+    const val architectureComponents = "2.2.0"
     const val buildTools = "29.0.0"
     const val cardView = "1.0.0"
     const val cling = "3.0.0"
