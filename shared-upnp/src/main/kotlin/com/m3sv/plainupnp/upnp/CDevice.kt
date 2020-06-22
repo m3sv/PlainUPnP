65a057ea2598145c881e91cd0d31f4307cd91563
m3sv <m3sv.dev@gmail.com>
2020-06-22 18:33:02 +0200

    Make device non-null

--- shared-upnp/src/main/kotlin/com/m3sv/plainupnp/upnp/CDevice.kt
+++ shared-upnp/src/main/kotlin/com/m3sv/plainupnp/upnp/CDevice.kt
@@ -6,56 +6,56 @@ import org.fourthline.cling.model.meta.LocalDevice
 import org.fourthline.cling.model.types.UDAServiceType
 import timber.log.Timber
 
-class CDevice(val device: Device<*, *, *>?) : UpnpDevice {
+class CDevice(val device: Device<*, *, *>) : UpnpDevice {
 
-    override val displayString: String = device?.displayString ?: ""
+    override val displayString: String = device.displayString
 
-    override val friendlyName: String = device?.details?.friendlyName ?: displayString
+    override val friendlyName: String = device.details.friendlyName ?: displayString
 
-    override val manufacturer: String = device?.details?.manufacturerDetails?.manufacturer ?: ""
+    override val manufacturer: String = device.details?.manufacturerDetails?.manufacturer ?: ""
 
     override val manufacturerUrl: String =
-        device?.details?.manufacturerDetails?.manufacturerURI?.toString() ?: ""
+        device.details?.manufacturerDetails?.manufacturerURI?.toString() ?: ""
 
-    override val modelName: String = device?.details?.modelDetails?.modelName ?: ""
+    override val modelName: String = device.details?.modelDetails?.modelName ?: ""
 
-    override val modelDesc: String = device?.details?.modelDetails?.modelDescription ?: ""
+    override val modelDesc: String = device.details?.modelDetails?.modelDescription ?: ""
 
-    override val modelNumber: String = device?.details?.modelDetails?.modelNumber ?: ""
+    override val modelNumber: String = device.details?.modelDetails?.modelNumber ?: ""
 
-    override val modelUrl: String = device?.details?.modelDetails?.modelURI?.toString() ?: ""
+    override val modelUrl: String = device.details?.modelDetails?.modelURI?.toString() ?: ""
 
-    override val xmlUrl: String = device?.details?.baseURL?.toString() ?: ""
+    override val xmlUrl: String = device.details?.baseURL?.toString() ?: ""
 
-    override val presentationURL: String = device?.details?.presentationURI?.toString() ?: ""
+    override val presentationURL: String = device.details?.presentationURI?.toString() ?: ""
 
-    override val serialNumber: String = device?.details?.serialNumber ?: ""
+    override val serialNumber: String = device.details?.serialNumber ?: ""
 
-    override val udn: String = device?.identity?.udn?.toString() ?: ""
+    override val udn: String = device.identity?.udn?.toString() ?: ""
 
-    override val uid: String = device?.identity?.udn?.toString() ?: ""
+    override val uid: String = device.identity?.udn?.toString() ?: ""
 
-    override val isFullyHydrated: Boolean = device?.isFullyHydrated ?: false
+    override val isFullyHydrated: Boolean = device.isFullyHydrated
 
     override val isLocal: Boolean = device is LocalDevice
 
     override val extendedInformation: String
         get() {
             val info = StringBuilder()
-            device?.findServiceTypes()?.forEach {
+            device.findServiceTypes()?.forEach {
                 info.append("\n\t").append(it.type).append(" : ").append(it.toFriendlyString())
             }
             return info.toString()
         }
 
     override fun equals(otherDevice: UpnpDevice?): Boolean =
-        device?.let {
-            it.identity.udn == (otherDevice as CDevice).device?.identity?.udn
-        } ?: false
+        device.let {
+            it.identity.udn == (otherDevice as CDevice).device.identity?.udn
+        }
 
 
     override fun printService() {
-        device?.findServices()?.forEach {
+        device.findServices()?.forEach {
             Timber.i("\t Service : $it")
             for (a in it.actions) {
                 Timber.i("\t\t Action : $a")
@@ -64,7 +64,7 @@ class CDevice(val device: Device<*, *, *>?) : UpnpDevice {
     }
 
     override fun asService(service: String): Boolean =
-        device?.findService(UDAServiceType(service)) != null
+        device.findService(UDAServiceType(service)) != null
 
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
@@ -72,12 +72,12 @@ class CDevice(val device: Device<*, *, *>?) : UpnpDevice {
 
         val cDevice = other as? CDevice
 
-        return device?.let {
+        return device.let {
             it == cDevice?.device
-        } ?: false
+        }
     }
 
-    override fun hashCode(): Int = device?.hashCode() ?: 0
+    override fun hashCode(): Int = device.hashCode()
 
     override fun toString(): String = "CDevice{ device=$device }"
 
