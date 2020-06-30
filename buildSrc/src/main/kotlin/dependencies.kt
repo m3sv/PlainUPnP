4430a4f7319bd2b92fd45a14d4cc8f280614bcdf
m3sv <m3sv.dev@gmail.com>
2020-06-30 20:32:44 +0200

    Bump version

--- buildSrc/src/main/kotlin/dependencies.kt
+++ buildSrc/src/main/kotlin/dependencies.kt
@@ -20,8 +20,8 @@ object Versions {
     const val sqlDelight = "1.4.0"
     const val targetSdk = 29
     const val test = "1.2.0"
-    const val versionCode = 61
-    const val versionName = "2.6.1"
+    const val versionCode = 62
+    const val versionName = "2.6.2"
 }
 
 object ClasspathDependencies {
