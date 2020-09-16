8ef80d54555d56cbe873f7e479885e96f5ef8c77
m3sv <m3sv.dev@gmail.com>
2020-09-16 20:14:17 +0200

    Release 2.8.2 (68)

--- buildSrc/src/main/kotlin/dependencies.kt
+++ buildSrc/src/main/kotlin/dependencies.kt
@@ -21,8 +21,8 @@ object Versions {
     const val sqlDelight = "1.4.0"
     const val targetSdk = 29
     const val test = "1.2.0"
-    const val versionCode = 66
-    const val versionName = "2.8.1"
+    const val versionCode = 68
+    const val versionName = "2.8.2"
 }
 
 object ClasspathDependencies {
