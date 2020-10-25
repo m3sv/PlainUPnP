a612c30fd1e2036afa8e6117c4a65ac23ea8b8e5
m3sv <m3sv.dev@gmail.com>
2020-10-25 17:59:17 +0100

    Move buildConfig to root build.gradle

--- buildSrc/src/main/kotlin/dependencies.kt
+++ buildSrc/src/main/kotlin/dependencies.kt
@@ -1,9 +1,7 @@
 object Versions {
     const val architectureComponents = "2.2.0"
-    const val buildTools = "29.0.3"
     const val cardView = "1.0.0"
     const val cling = "2.2.0"
-    const val compileSdk = 29
     const val constraintLayout = "2.0.0"
     const val coroutines = "1.3.9"
     const val coreTesting = "2.1.0"
@@ -14,15 +12,11 @@ object Versions {
     const val kotlin = "1.4.10"
     const val okHttp = "4.9.0"
     const val material = "1.2.0"
-    const val minSdk = 21
     const val nanohttpd = "2.3.1"
     const val recyclerView = "1.1.0"
     const val supportLibrary = "1.1.0"
     const val sqlDelight = "1.4.0"
-    const val targetSdk = 29
     const val test = "1.2.0"
-    const val versionCode = 68
-    const val versionName = "2.8.2"
 }
 
 object Dependencies {
