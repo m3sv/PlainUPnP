e1a14d57023c5efd18863ef5d588f818adcee2c0
m3sv <m3sv.dev@gmail.com>
2021-04-24 13:47:15 +0200

    [WIP] Migrate to version catalogs, compose

---
+++ settings.gradle.kts
@@ -0,0 +1,16 @@
+enableFeaturePreview("VERSION_CATALOGS")
+
+include(
+    ":app",
+    ":common",
+    ":core:eventbus",
+    ":core:persistence",
+    ":core:resources",
+    ":feature:home",
+    ":feature:inappplayer",
+    ":feature:onboarding",
+    ":feature:selectcontentdirectory",
+    ":feature:settings",
+    ":shared:upnp",
+)
+
