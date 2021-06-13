73b6cdd4af4f685f44f8654a8cbb3050f75f3223
m3sv <m3sv.dev@gmail.com>
2021-06-13 21:48:41 +0200

    Remove redundant stateIn

--- common/src/main/kotlin/com/m3sv/plainupnp/ThemeManager.kt
+++ common/src/main/kotlin/com/m3sv/plainupnp/ThemeManager.kt
@@ -5,9 +5,7 @@ import androidx.compose.runtime.State
 import androidx.compose.runtime.collectAsState
 import com.m3sv.plainupnp.common.preferences.PreferencesRepository
 import kotlinx.coroutines.MainScope
-import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -17,10 +15,7 @@ class ThemeManager @Inject constructor(private val preferencesRepository: Prefer
 
     private val scope = MainScope()
 
-    private val theme: StateFlow<ThemeOption> =
-        preferencesRepository
-            .theme
-            .stateIn(scope, SharingStarted.Eagerly, ThemeOption.System)
+    private val theme: StateFlow<ThemeOption> = preferencesRepository.theme
 
     fun setTheme(mode: ThemeOption) {
         scope.launch { preferencesRepository.setApplicationTheme(mode) }
